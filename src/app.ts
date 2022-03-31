0db5b37a02a807fa9ec9ed8463e0fda12517e75b
mspano <developer.web.ms@gmail.com>
2022-03-31 11:16:22 -0300

    Settings for productive environment

--- src/app.ts
+++ src/app.ts
@@ -84,9 +84,9 @@ export class App {
               // - Write all logs with level `error` and below to `error.log`
               // - Write all logs with level `info` and below to `combined.log`
               //
-              new winston.transports.File({ filename: 'error.log', level: 'error' , maxsize: 2000000 }),
+              // new winston.transports.File({ filename: 'error.log', level: 'error' , maxsize: 2000000 }),
               new winston.transports.File({ filename: 'warning.log', level: 'warn' , maxsize: 2000000 }),
-              new winston.transports.File({ filename: 'info.log', level: 'info' , maxsize: 2000000 }),
+              // new winston.transports.File({ filename: 'info.log', level: 'info' , maxsize: 2000000 }),
             ],
           });
           if (this.configuration.env === DEV) {
